6bfdb1eee1184f9e90a95e27ba9dcf8d3710819c
wangse9527 <118261250+wangse9527@users.noreply.github.com>
2023-01-14 16:33:55 +0800

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/v2ray/config.json
+sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/nginx/nginx.conf
+exec "$@"
